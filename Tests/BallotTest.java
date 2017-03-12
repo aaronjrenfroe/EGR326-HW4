6c14ba835c6cf5c1a1ebfd50d97c8dfa82a10bc5
AaronR <aaron@afterrabble.com>
2017-03-11 21:07:03 -0800

    Final Commit

---
+++ Tests/BallotTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AaronR on 3/9/17.
+ * for ?
+ */
+public class BallotTest {
+    @Test
+    public void getVoteTest() throws Exception {
+        Ballot b = new Ballot();
+        b.addVotes(new LinkedList<String>());
+
+    }
+
+    @Test
+    public void deleteCandidateVotesTest() throws Exception {
+
+    }
+
+    @Test
+    public void nextVoteTest() throws Exception {
+
+    }
+
+    @Test
+    public void toStringTest() throws Exception {
+
+    }
+
+}
---
+++ Tests/ElectionTest.java
@@ -0,0 +1,33 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AaronR on 3/11/17.
+ * for ?
+ */
+public class ElectionTest {
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void addPollingPlace() throws Exception {
+        Election n = Election.getInstance();
+        n.addPollingPlace("bellevue");
+        n.addPollingPlace("kent");
+        n.closePollingPlaces();
+        Boolean threwError = false;
+        try {
+            n.addPollingPlace("capitol-hill");
+        }catch(UnsupportedOperationException uoe){
+            threwError = true;
+        }
+        Assert.assertTrue("Can't add polling place while polls are closed.",threwError);
+
+    }
+
+}
